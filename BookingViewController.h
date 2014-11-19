d0ff481fc6dd76045a0aa1cdc0334e91eb7b4a5c
talaa <tamer.alaaeldine@gmail.com>
2014-11-19 20:13:01 +0400

    Most updated 19112014 With booking view controller Added

---
+++ BookingViewController.h
@@ -0,0 +1,13 @@
+//
+//  BookingViewController.h
+//  Belle De Jour
+//
+//  Created by Tamer Alaa on 11/19/14.
+//  Copyright (c) 2014 SpringMoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookingViewController : UIViewController
+
+@end
