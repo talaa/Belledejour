1708b20e443cf3e0d08b738bf0cde3d458978f9d
nadagamal <nadagamal_ng@yahoo.com>
2015-02-09 20:12:48 +0200

    Last Phase

--- Belle De Jour/FeedbackViewController.h
+++ Belle De Jour/FeedbackViewController.h
@@ -9,13 +9,17 @@
 #import <UIKit/UIKit.h>
 #import <MessageUI/MessageUI.h>
 
-@interface FeedbackViewController : UIViewController<MFMailComposeViewControllerDelegate,UIPickerViewDelegate,UIPickerViewDataSource>
+@interface FeedbackViewController : UIViewController<MFMailComposeViewControllerDelegate,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate>
 @property (nonatomic,strong)IBOutlet UITextField *messagebody;
 @property (nonatomic,strong)IBOutlet UITextField *messagetitle;
 @property (nonatomic,strong)IBOutlet UITextField *serviceratingfield;
 @property (nonatomic,strong)IBOutlet UITextField *overallratingfield;
 @property (nonatomic,strong)IBOutlet UISwitch *recommendationfield;
 @property (nonatomic,strong)IBOutlet UIButton *sendmessage;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTxt;
+- (IBAction)switchChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *phoneNumberLbl;
 -(IBAction)sendmessage:(id)sender;
 -(IBAction)servrating:(id)sender;
 -(IBAction)overallrating:(id)sender;
--- Belle De Jour/FeedbackViewController.m
+++ Belle De Jour/FeedbackViewController.m
@@ -9,10 +9,17 @@
 #import "FeedbackViewController.h"
 
 @interface FeedbackViewController ()
+{
+    UIPickerView * serviceRatingPickerView;
+    UIPickerView * overAllRatingPickerView;
+
+    UIToolbar *toolBar;
+}
 @property (nonatomic)NSArray *servicerating;
 @property (nonatomic)NSArray *overallrating;
 @property (nonatomic)NSArray *recommendyesno;
 @property (nonatomic)int indexpicker;
+
 
 @end
 
@@ -20,6 +27,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  serviceRatingPickerView = [[UIPickerView alloc] init];
+    overAllRatingPickerView = [[UIPickerView alloc] init];
+
     _servicerating=@[@"Excellent",@"Very Good",@"Good",@"Fair"];
     _overallrating=@[@"Excellent",@"Very Good",@"Good",@"Fair"];
     _recommendyesno=@[@"YES",@"NO"];
@@ -27,6 +37,19 @@
     [_sendmessage.layer setCornerRadius:35.0f];
     [_sendmessage.layer setMasksToBounds:YES];
     self.title=@"Feedback";
+    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height+150)];
+    toolBar= [[UIToolbar alloc] initWithFrame:CGRectMake(0,0,[[UIScreen mainScreen]bounds].size.width,44)];
+    [toolBar setBarStyle:UIBarStyleBlackTranslucent];
+    UIBarButtonItem *barButtonDone = [[UIBarButtonItem alloc] initWithTitle:@"Done"
+                                                                      style:UIBarButtonItemStyleBordered target:self action:@selector(doneToolbarPressed:)];
+    toolBar.items = [[NSArray alloc] initWithObjects:barButtonDone,nil];
+    
+ 
+    
+
+    barButtonDone.tintColor=[UIColor blackColor];
+  //  [serviceRatingPickerView addSubview:toolBar];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,21 +58,29 @@
 }
 -(IBAction)sendmessage:(id)sender{
     NSString *recommendation=[[NSString alloc]init];
-    if (_recommendationfield.state) {
+    if (_recommendationfield.isOn) {
         recommendation=@"Yes ,i will recommend you to my friends ";
     }else{
     recommendation=@"Sorry , but i will not recommend you";
     }
 
-    MFMailComposeViewController *picker=[[MFMailComposeViewController alloc]init];
-    picker.mailComposeDelegate=self;
-    [picker setSubject:_messagetitle.text ];
-    [picker setToRecipients:@[@"info@belledejour.ae"]];
-    NSString *messagebodytext=[[NSString alloc]initWithFormat:@"I have Found the Service %@ \nand over all rating of the clinic %@ \n and %@ \nHere is My Feedback: %@",_serviceratingfield.text,_overallratingfield.text,recommendation,_messagebody.text,nil];
-    [picker setMessageBody:messagebodytext isHTML:NO];
+    MFMailComposeViewController * messageScreen=[[MFMailComposeViewController alloc]init];
+    messageScreen.mailComposeDelegate=self;
+    [messageScreen setSubject:_messagetitle.text ];
+    [messageScreen setToRecipients:@[@"info@belledejour.ae"]];
+    NSString *messagebodytext;
+    if(!_recommendationfield.isOn)
+    {
+   messagebodytext=[[NSString alloc]initWithFormat:@"I have Found the Service %@ \nand over all rating of the clinic %@ \n and %@ \nHere is My Feedback: %@",_serviceratingfield.text,_overallratingfield.text,recommendation,_messagebody.text,nil];
+    }
+    else
+    {
+           messagebodytext=[[NSString alloc]initWithFormat:@"I have Found the Service %@ \nand over all rating of the clinic %@ \n and %@ Friend mobile number is : %@  \nHere is My Feedback: %@",_serviceratingfield.text,_overallratingfield.text,recommendation, _phoneNumberTxt.text,_messagebody.text,nil];
+    }
+    [messageScreen setMessageBody:messagebodytext isHTML:NO];
     //[picker addAttachmentData:[self.detailItem valueForKey:@"image"] mimeType:@"image/png" fileName:@"Photos"];
-    [self dismissViewControllerAnimated:YES completion:NULL];
-    [self presentViewController:picker animated:YES completion:nil];
+    //[self dismissViewControllerAnimated:YES completion:NULL];
+    [self presentViewController:messageScreen animated:YES completion:nil];
 
 
 
@@ -82,25 +113,47 @@
 
 
 }
--(IBAction)servrating:(id)sender;{
+-(IBAction)servrating:(id)sender{
     //[_serviceratingfield resignFirstResponder];
-    UIPickerView *picker1 = [[UIPickerView alloc] init];
-    
-    picker1.dataSource = self;
-    picker1.delegate = self;
-    picker1.showsSelectionIndicator=YES;
+    serviceRatingPickerView.hidden=NO;
+
+    serviceRatingPickerView.dataSource = self;
+    serviceRatingPickerView.delegate = self;
+    serviceRatingPickerView.showsSelectionIndicator=YES;
     _indexpicker=1;
-    _serviceratingfield.inputView=picker1;
+    _serviceratingfield.inputView=serviceRatingPickerView;
+    _serviceratingfield.inputAccessoryView=toolBar;
+    [serviceRatingPickerView reloadComponent:0];
+
+
+
+}
+-(void)doneToolbarPressed:(id)sender
+{
+    serviceRatingPickerView.hidden=YES;
+    overAllRatingPickerView.hidden=YES;
+
+    [_serviceratingfield endEditing:YES];
+    [_overallratingfield endEditing:YES];
+
+    
 }
 -(IBAction)overallrating:(id)sender{
+   // serviceRatingPickerView = [[UIPickerView alloc] init];
+    overAllRatingPickerView.hidden=NO;
+
     //[_overallratingfield resignFirstResponder];
-    UIPickerView *picker1 = [[UIPickerView alloc] init];
-    
-    picker1.dataSource = self;
-    picker1.delegate = self;
-    picker1.showsSelectionIndicator=YES;
+  //  UIPickerView *picker1 = [[UIPickerView alloc] init];
+    overAllRatingPickerView.dataSource = self;
+    overAllRatingPickerView.delegate = self;
+    overAllRatingPickerView.showsSelectionIndicator=YES;
     _indexpicker=2;
-    _overallratingfield.inputView=picker1;
+    _overallratingfield.inputView=overAllRatingPickerView;
+    _overallratingfield.inputAccessoryView=toolBar;
+   // [overAllRatingPickerView reloadInputViews];
+   // [overAllRatingPickerView reloadComponent:0];
+
+   [overAllRatingPickerView reloadComponent:0];
 
 
 
@@ -108,8 +161,13 @@
 }
 //Prepare th UI
 -(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if(pickerView==serviceRatingPickerView)
     return [_servicerating count];
-    
+    else if (pickerView==overAllRatingPickerView)
+        return [_overallrating count];
+
+    else
+        return 0;
 }
 
 -(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
@@ -117,13 +175,12 @@
 }
 
 -(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
-    
-    if (_indexpicker==1) {
-        return _servicerating[row];
-    }
-    else{
+    if(pickerView==serviceRatingPickerView)
+    return _servicerating[row];
+    else if (pickerView==overAllRatingPickerView)
         return _overallrating[row];
-    }
+    else
+    return nil;
     
 }
 
@@ -139,7 +196,22 @@
     [self dismissViewControllerAnimated:YES completion:nil];
         
 }
+#pragma textField Delegate
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height+160)];
 
+}
+-(void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height-160)];
+
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.scrollView endEditing:YES];
+    return YES;
+}
 /*
 #pragma mark - Navigation
 
@@ -150,4 +222,17 @@
 }
 */
 
+- (IBAction)switchChanged:(id)sender {
+    if(_recommendationfield.isOn)
+    {
+        _phoneNumberLbl.hidden=NO;
+        _phoneNumberTxt.hidden=NO;
+    }
+    else
+    {
+        _phoneNumberLbl.hidden=YES;
+        _phoneNumberTxt.hidden=YES;
+        
+    }
+}
 @end
